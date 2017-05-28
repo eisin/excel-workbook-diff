aac5f1d85c8e10e709748a83c860e752b7086616
EC2 Default User <eisin@users.noreply.github.com>
2017-05-28 07:59:03 +0000

    modified excel-workbook-diff.py

--- excel-workbook-diff.py
+++ excel-workbook-diff.py
@@ -1,5 +1,6 @@
 import sys
 import openpyxl
+from difflib import SequenceMatcher
 
 def excel_workbook_diff(file1, file2, title_row):
     print("file:{} {}".format(file1, file2))
@@ -7,22 +8,37 @@ def excel_workbook_diff(file1, file2, title_row):
     sheet2 = read_workbook(file2, 0)
     
     title_row_cells = []
-    for cell in sheet1.iter_cols(min_row=title_row, max_row=title_row):
-        title = cell[0].value
-        if title is None:
-            title = ""
-        title.replace("\n", "")
-        title_row_cells.append(title)
     sheet2_title_row_cells = []
-    for cell in sheet2.iter_cols(min_row=title_row, max_row=title_row):
-        title = cell[0].value
-        if title is None:
-            title = ""
-        title.replace("\n", "")
-        sheet2_title_row_cells.append(title)
+    for cells in sheet1.iter_cols(min_row=title_row, max_row=title_row):
+        title_row_cells.append(cell_to_text_oneline(cells[0]))
+    for cells in sheet2.iter_cols(min_row=title_row, max_row=title_row):
+        sheet2_title_row_cells.append(cell_to_text_oneline(cells[0]))
     if title_row_cells != sheet2_title_row_cells:
         raise Exception("title fields not matched. {} vs {}", title_row_cells, sheet2_title_row_cells)
     print (",".join(title_row_cells))
+
+    table1 = read_sheet_table(sheet1)
+    table2 = read_sheet_table(sheet2)
+
+    matcher = SequenceMatcher(None, table1, table2)
+    #print(matcher)
+    #print(list(matcher.get_opcodes()))
+    for tag, i1, i2, j1, j2 in matcher.get_opcodes():
+        print(tag, i1, i2, j1, j2)
+        if tag == "equal":
+            pass
+        elif tag == "insert":
+            for i in range(j1, j2):
+                print("insert #{}:{}".format(i, table2[i]))
+        elif tag == "delete":
+            for i in range(i1, i2):
+                print("delete #{}:{}".format(i, table2[i]))
+        elif tag == "replace":
+            for i in range(i1, i2):
+                print("delete #{}:{}".format(i, table2[i]))
+            for i in range(j1, j2):
+                print("insert #{}:{}".format(i, table2[i]))
+        
 
 def read_workbook(filename, sheet_index):
     workbook = openpyxl.load_workbook(filename, data_only=True)
@@ -31,6 +47,29 @@ def read_workbook(filename, sheet_index):
     else:
         worksheet = workbook.get_sheet_by_name(sheet_index)
     return worksheet
+
+def read_sheet_table(sheet, start_row=2):
+    row_index = 0
+    table = []
+    for row in sheet.rows:
+        row_index += 1
+        if row_index <= start_row:
+            continue
+        line = []
+        for cell in row:
+            text = cell.value
+            if text is None:
+                text = ""
+            line.append(str(text))
+        table.append(tuple(line))
+    return table
+
+def cell_to_text_oneline(cell):
+    text = cell.value
+    if text is None:
+        text = ""
+    text.replace("\n", "")
+    return text
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:

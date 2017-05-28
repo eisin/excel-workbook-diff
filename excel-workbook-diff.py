bd1d33b7334f9d3446ec249d725af2ffec7aa1e1
EC2 Default User <eisin@users.noreply.github.com>
2017-05-28 11:36:53 +0000

    implemented one-by-one diff

--- excel-workbook-diff.py
+++ excel-workbook-diff.py
@@ -1,9 +1,9 @@
 import sys
 import openpyxl
 from difflib import SequenceMatcher
+from difflib import Differ
 
 def diff_excel_workbook(file1, file2, title_row):
-    print("file:{} {}".format(file1, file2))
     sheet1 = read_workbook(file1, 0)
     sheet2 = read_workbook(file2, 0)
     
@@ -18,18 +18,46 @@ def diff_excel_workbook(file1, file2, title_row):
 
     table1 = read_sheet_table(sheet1)
     table2 = read_sheet_table(sheet2)
-    diff_result = diff_two_tables(table1, table2)
 
+    diff_result = diff_two_tables(table1, table2)
+    return diff_result, sheet1_header_titles
+
+
+def format_diff_two_tables(diff_result, header_titles, primary_key_columns, prefix_row="=== ", prefix_column="# "):
+    format_result = ""
     for opcode, field1, field2 in diff_result:
+        format_result += prefix_row + " ".join([field1[i] for i in primary_key_columns]) + "\n"
         if opcode == "insert":
-            print("INSERT:" + ",".join(field1))
+            for i in range(0, len(field1)):
+                format_result += prefix_column + header_titles[i] + "\n"
+                format_result += "+" + field1[i] + "\n"
         elif opcode == "delete":
-            print("DELETE:" + ",".join(field1))
+            for i in range(0, len(field1)):
+                format_result += prefix_column + header_titles[i] + "\n"
+                format_result += "-" + field1[i] + "\n"
         elif opcode == "replace":
-            print("DELREP:" + ",".join(field1))
-            print("INSREP:" + ",".join(field2))
+            for i in range(0, len(field1)):
+                if field1[i] == field2[i]:
+                    continue
+                format_result += prefix_column + header_titles[i] + "\n"
+                field1array = field1[i].splitlines()
+                field2array = field2[i].splitlines()
+                match = SequenceMatcher(None, field1array, field2array)
+                for tag, i1, i2, j1, j2 in match.get_opcodes():
+                    if tag == "equal":
+                        for line in field1array[i1:i2]:
+                            format_result += " " + line + "\n"
+                    if tag == "delete" or tag == "replace":
+                        for line in field1array[i1:i2]:
+                            format_result += "-" + line + "\n"
+                    if tag == "insert" or tag == "replace":
+                        for line in field2array[j1:j2]:
+                            format_result += "+" + line + "\n"
+            for i in range(len(field1), len(field2)):
+                format_result += "+" + field2[i] + "\n"
         else:
             raise Exception("Could not recognize diff opcode:{}".format(opcode))
+    return format_result
 
 def diff_two_tables(table1, table2):
     matcher = SequenceMatcher(None, table1, table2)
@@ -39,34 +67,36 @@ def diff_two_tables(table1, table2):
             pass
         elif tag == "insert":
             for i in range(j1, j2):
-                result.append(("insert", table2[i],None,))
+                result.append(("insert", table2[i], None,))
         elif tag == "delete":
             for i in range(i1, i2):
-                result.append(("delete", table1[i],None,))
+                result.append(("delete", table1[i], None,))
         elif tag == "replace":
             insert_entries = list(table2[j1:j2])
             for i in range(i1, i2):
                 while True:
                     replace_one_line_score = 0
+                    insert_one_line_score = 0
+                    delete_one_line_score = 0
                     if len(insert_entries) >= 1:
                         replace_one_line_score = count_exact_entries_in_tuple(table1[i], insert_entries[0])
-                    insert_one_line_score = 0
                     if len(insert_entries) >= 2:
                         insert_one_line_score = count_exact_entries_in_tuple(table1[i], insert_entries[1])
-                    delete_one_line_score = 0
-                    if i < i2 and len(insert_entries) >= 1:
+                    if i < i2 - 1 and len(insert_entries) >= 1:
                         delete_one_line_score = count_exact_entries_in_tuple(table1[i + 1], insert_entries[0])
-                    print("SCORE replace({}) insert({}) delete({})".format(replace_one_line_score, insert_one_line_score, delete_one_line_score))
-                    if max(replace_one_line_score, insert_one_line_score, delete_one_line_score) == replace_one_line_score:
-                        #print("change(before) #{}:{}".format(i, table1[i]))
-                        #print("change(after) #{}:{}".format(i, insert_entries.pop(0)))
+                    if replace_one_line_score == 0 and insert_one_line_score == 0 and delete_one_line_score == 0:
+                        result.append(("delete", table1[i], None,))
+                        break
+                    elif max(replace_one_line_score, insert_one_line_score, delete_one_line_score) == replace_one_line_score:
                         result.append(("replace", table1[i], insert_entries.pop(0)))
                         break
                     elif max(replace_one_line_score, insert_one_line_score, delete_one_line_score) == insert_one_line_score:
-                        result.append(("insert", table2[i],None,))
+                        result.append(("insert", insert_entries.pop(0), None,))
                     else:
-                        result.append(("delete", table1[i],None,))
+                        result.append(("delete", table1[i], None,))
                         break
+            for entry in insert_entries:
+                result.append(("insert", entry, None,))
     return result
         
 def count_exact_entries_in_tuple(tuple1, tuple2):
@@ -112,4 +142,27 @@ if __name__ == '__main__':
         raise Exception("Invalid arguments")
     file1 = sys.argv[1]
     file2 = sys.argv[2]
-    diff_excel_workbook(file1, file2, 1)
+    diff_result, header_titles = diff_excel_workbook(file1, file2, 1)
+    text = format_diff_two_tables(diff_result, header_titles, (0, 2,))
+    print(text)
+    
+    exit(0)
+    table1 = [
+        ("1", "col1-1", "col1-2", "col1-3", "col1-4"),
+        ("2", "col2-1", "col2-2", "col2-3", "col2-4"),
+        ("3", "col3-2", "col3-2", "col3-3", "col3-4"),
+    ]
+    table2 = [
+        ("1", "col1-1", "col1-2", "col1-3", "col1-4"),
+        ("2", "col2-1", "col2-2", "col2-3", "col2-4"),
+        #("1-1", "col2-1", "col2-2", "col2-3", "col1-1-4"),
+        ("2", "col2-1", "col2-2", "col2-3", "col2-4"),
+        #("2", "col2-0", "col2-2", "col2-3", "col2-4"),
+        #("2", "col2-0", "col2-2", "col2-3", "col2-4"),
+        #("1-1", "col2-1", "col2-2", "col2-3", "col1-1-4"),
+        #("3", "col3-1", "col3-2", "col3-3", "col3-4"),
+    ]
+    result = diff_two_tables(table1, table2)
+    import pprint
+    pp = pprint.PrettyPrinter()
+    pp.pprint(result)

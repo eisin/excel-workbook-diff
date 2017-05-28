100fdb2a97b3c1a599ba745efe63cf7cbca86d17
EC2 Default User <eisin@users.noreply.github.com>
2017-05-28 09:59:35 +0000

    implemented one-by-one diff

--- excel-workbook-diff.py
+++ excel-workbook-diff.py
@@ -2,34 +2,47 @@ import sys
 import openpyxl
 from difflib import SequenceMatcher
 
-def excel_workbook_diff(file1, file2, title_row):
+def diff_excel_workbook(file1, file2, title_row):
     print("file:{} {}".format(file1, file2))
     sheet1 = read_workbook(file1, 0)
     sheet2 = read_workbook(file2, 0)
     
-    title_row_cells = []
-    sheet2_title_row_cells = []
+    sheet1_header_titles = []
+    sheet2_header_titles = []
     for cells in sheet1.iter_cols(min_row=title_row, max_row=title_row):
-        title_row_cells.append(cell_to_text_oneline(cells[0]))
+        sheet1_header_titles.append(cell_to_text_oneline(cells[0]))
     for cells in sheet2.iter_cols(min_row=title_row, max_row=title_row):
-        sheet2_title_row_cells.append(cell_to_text_oneline(cells[0]))
-    if title_row_cells != sheet2_title_row_cells:
-        raise Exception("title fields not matched. {} vs {}", title_row_cells, sheet2_title_row_cells)
-    print (",".join(title_row_cells))
+        sheet2_header_titles.append(cell_to_text_oneline(cells[0]))
+    if sheet1_header_titles != sheet2_header_titles:
+        raise Exception("title fields not matched. {} vs {}", sheet1_header_titles, sheet2_header_titles)
 
     table1 = read_sheet_table(sheet1)
     table2 = read_sheet_table(sheet2)
+    diff_result = diff_two_tables(table1, table2)
 
+    for opcode, field1, field2 in diff_result:
+        if opcode == "insert":
+            print("INSERT:" + ",".join(field1))
+        elif opcode == "delete":
+            print("DELETE:" + ",".join(field1))
+        elif opcode == "replace":
+            print("DELREP:" + ",".join(field1))
+            print("INSREP:" + ",".join(field2))
+        else:
+            raise Exception("Could not recognize diff opcode:{}".format(opcode))
+
+def diff_two_tables(table1, table2):
     matcher = SequenceMatcher(None, table1, table2)
+    result = []
     for tag, i1, i2, j1, j2 in matcher.get_opcodes():
         if tag == "equal":
             pass
         elif tag == "insert":
             for i in range(j1, j2):
-                print("insert #{}:{}".format(i, table2[i]))
+                result.append(("insert", table2[i],None,))
         elif tag == "delete":
             for i in range(i1, i2):
-                print("delete #{}:{}".format(i, table1[i]))
+                result.append(("delete", table1[i],None,))
         elif tag == "replace":
             insert_entries = list(table2[j1:j2])
             for i in range(i1, i2):
@@ -45,15 +58,16 @@ def excel_workbook_diff(file1, file2, title_row):
                         delete_one_line_score = count_exact_entries_in_tuple(table1[i + 1], insert_entries[0])
                     print("SCORE replace({}) insert({}) delete({})".format(replace_one_line_score, insert_one_line_score, delete_one_line_score))
                     if max(replace_one_line_score, insert_one_line_score, delete_one_line_score) == replace_one_line_score:
-                        print("change(before) #{}:{}".format(i, table1[i]))
-                        print("change(after) #{}:{}".format(i, insert_entries.pop(0)))
+                        #print("change(before) #{}:{}".format(i, table1[i]))
+                        #print("change(after) #{}:{}".format(i, insert_entries.pop(0)))
+                        result.append(("replace", table1[i], insert_entries.pop(0)))
                         break
                     elif max(replace_one_line_score, insert_one_line_score, delete_one_line_score) == insert_one_line_score:
-                        print("insert #{}:{}".format(i, insert_entries.pop(0)))
+                        result.append(("insert", table2[i],None,))
                     else:
-                        print("delete #{}:{}".format(i, table1[i]))
+                        result.append(("delete", table1[i],None,))
                         break
-            
+    return result
         
 def count_exact_entries_in_tuple(tuple1, tuple2):
     count = 0
@@ -98,4 +112,4 @@ if __name__ == '__main__':
         raise Exception("Invalid arguments")
     file1 = sys.argv[1]
     file2 = sys.argv[2]
-    excel_workbook_diff(file1, file2, 1)
+    diff_excel_workbook(file1, file2, 1)
